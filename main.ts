05c1bedd65c6dbae8b517db5322f778111a085fa
adriandegonda <adrian.degonda@gmail.com>
2025-06-22 23:03:32 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,4 +13,21 @@ namespace meineErweiterung {
         let neuerWert = wert + 1000;
         serial.writeLine("Gesendeter Wert: " + neuerWert);
     }
+
+
+        /**
+     * Sende eine Zahl über die serielle Schnittstelle im AMBAR-Format.
+     * @param value die Zahl, die gesendet werden soll
+     * @param channel der Kanal (A-E) über den gesendet wird
+     */
+    //% block="sende Zahl %value an Kanal %channel"
+    //% value.min=0 value.max=1000
+    //% color=#cd7f32 weight=100
+    export function sendNumber(value: number, channel: Channel): void {
+        serial.setBaudRate(BaudRate.BaudRate57600)  // Baudrate auf 57600 setzen:contentReference[oaicite:0]{index=0}
+        let chLetter = channelToLetter(channel)
+        serial.writeString("s" + chLetter + value + "e")
+    }
+
+    
 }
